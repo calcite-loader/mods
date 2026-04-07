421ab292a78cdba318939eecd13084523a792ba1
Grady Link <codeberg@grady.link>
2026-04-07 08:07:48 -0700

    feat: add current percent mod

---
+++ src/currentPercent.ts
@@ -0,0 +1,39 @@
+/*
+ * @name Current Percent
+ */
+
+const settings = api.registerSettings({
+  decimals: {
+    type: "toggle",
+    name: "Show Decimals",
+    default: false,
+  },
+});
+
+api.onLoad(() => {
+  const originalStartGame = window.gdScene._startGame;
+
+  window.gdScene._startGame = () => {
+    const percentText = window.gdScene.add.bitmapText(
+      window.gdScene.cameras.main.width / 2,
+      20,
+      "bigFont",
+      "0%",
+      32,
+    ).setOrigin(0.5, 0);
+
+    window.gdScene.events.on("update", () => {
+      percentText.setText(
+        Math.max(
+          Math.floor(
+            window.gdScene._playerWorldX / window.gdScene._level.endXPos *
+              (settings.decimals ? 1000 : 100),
+          ) / (settings.decimals ? 10 : 1),
+          0,
+        ).toFixed(settings.decimals ? 1 : 0) + "%",
+      );
+    });
+
+    originalStartGame.call(window.gdScene);
+  };
+});
