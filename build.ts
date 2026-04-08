106cab5cd845a597e1b270f55afb1a6bd3d9fc05
Grady Link <github@grady.link>
2026-04-07 18:21:33 -0700

    feat: init deploy action

--- build.ts
+++ build.ts
@@ -9,13 +9,18 @@ const extractHeader = async (filePath: string): Promise<string> => {
   return headerMatch && headerMatch[1] ? headerMatch[1] : "";
 };
 
+const isCI = Bun.argv.includes("--ci");
+const manifest = {
+  mods: [] as { id: string; name: string; downloadUrl: string }[],
+};
+
 (async () => {
   const files = [...new Glob("*.ts").scanSync("src/")];
 
   for (const file of files) {
+    const id = path.parse(file).name;
     const inputPath = path.join("src", file);
-    const fileName = path.basename(file, ".ts");
-    const outputPath = path.join("dist", `${fileName}.js`);
+    const outputPath = path.join("dist", `${id}.js`);
 
     const result = await Bun.build({
       entrypoints: [inputPath],
@@ -33,9 +38,28 @@ const extractHeader = async (filePath: string): Promise<string> => {
     if (header) {
       const builtContent = await Bun.file(outputPath).text();
       await Bun.write(outputPath, header + "\n\n" + builtContent);
-    }
 
-    console.log("Built " + file);
+      const nameMatch = header.match(/@name\s+(.*)/);
+      if (isCI) {
+        manifest.mods.push({
+          id,
+          name: nameMatch ? nameMatch[1]!.trim() : id,
+          downloadUrl:
+            `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${process.env.GITHUB_REPOSITORY_NAME}/${id}.js`,
+        });
+      }
+
+      console.log(
+        "Built: " + (nameMatch ? nameMatch[1]?.trim() : id),
+      );
+    } else {
+      console.log("Built: " + id);
+    }
+  }
+
+  if (isCI) {
+    await Bun.write("dist/manifest.json", JSON.stringify(manifest));
+    console.log("Generated Manifest!");
   }
 
   console.log("Build Successful!");
