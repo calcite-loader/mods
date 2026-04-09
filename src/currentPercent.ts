98e3d8b94eacd72cdfb55fb34d9090237affdbfc
Grady Link <github@grady.link>
2026-04-08 19:57:01 -0700

    refactor: use new events

--- src/currentPercent.ts
+++ src/currentPercent.ts
@@ -10,30 +10,24 @@ const settings = api.registerSettings({
   },
 });
 
-api.onLoad(() => {
-  const originalStartGame = window.gdScene._startGame;
+api.onStart(() => {
+  const percentText = window.gdScene.add.bitmapText(
+    window.gdScene.cameras.main.width / 2,
+    20,
+    "bigFont",
+    "0%",
+    32,
+  ).setOrigin(0.5, 0);
 
-  window.gdScene._startGame = () => {
-    const percentText = window.gdScene.add.bitmapText(
-      window.gdScene.cameras.main.width / 2,
-      20,
-      "bigFont",
-      "0%",
-      32,
-    ).setOrigin(0.5, 0);
-
-    window.gdScene.events.on("update", () => {
-      percentText.setText(
-        Math.max(
-          Math.floor(
-            window.gdScene._playerWorldX / window.gdScene._level.endXPos *
-              (settings.decimals ? 1000 : 100),
-          ) / (settings.decimals ? 10 : 1),
-          0,
-        ).toFixed(settings.decimals ? 1 : 0) + "%",
-      );
-    });
-
-    originalStartGame.call(window.gdScene);
-  };
+  window.gdScene.events.on("update", () => {
+    percentText.setText(
+      Math.max(
+        Math.floor(
+          window.gdScene._playerWorldX / window.gdScene._level.endXPos *
+            (settings.decimals ? 1000 : 100),
+        ) / (settings.decimals ? 10 : 1),
+        0,
+      ).toFixed(settings.decimals ? 1 : 0) + "%",
+    );
+  });
 });
