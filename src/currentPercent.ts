99ccf172e0d41758cbc2d21a2f175aff30227169
Grady Link <github@grady.link>
2026-04-10 18:37:08 -0700

    refactor: switch to use on update

--- src/currentPercent.ts
+++ src/currentPercent.ts
@@ -10,27 +10,29 @@ const settings = api.registerSettings({
   },
 });
 
+let percentText: Phaser.GameObjects.BitmapText;
+
 api.onStart(() => {
-  const percentText = window.gdScene.add.bitmapText(
+  percentText = window.gdScene.add.bitmapText(
     window.gdScene.cameras.main.width / 2,
     20,
     "bigFont",
     "0%",
     32,
   ).setOrigin(0.5, 0);
+});
 
-  window.gdScene.events.on("update", () => {
-    const places = Math.max(0, settings.decimals);
-    const multiplier = Math.pow(10, places);
+api.onUpdate(() => {
+  const places = Math.max(0, settings.decimals);
+  const multiplier = Math.pow(10, places);
 
-    const progress =
-      (window.gdScene._playerWorldX / window.gdScene._level.endXPos) * 100;
+  const progress =
+    (window.gdScene._playerWorldX / window.gdScene._level.endXPos) * 100;
 
-    const formattedPercent = (Math.max(
-      Math.floor(progress * multiplier) / multiplier,
-      0,
-    )).toFixed(places);
+  const formattedPercent = (Math.max(
+    Math.floor(progress * multiplier) / multiplier,
+    0,
+  )).toFixed(places);
 
-    percentText.setText(formattedPercent + "%");
-  });
+  percentText.setText(formattedPercent + "%");
 });
--- src/hitboxes.ts
+++ src/hitboxes.ts
@@ -3,22 +3,22 @@
  * @needsRefresh true
  */
 
+let hitboxGraphics: Phaser.GameObjects.Graphics;
+
 api.onLoad(() => {
-  (window as any).hitboxGraphics = window.gdScene.add.graphics()
+  hitboxGraphics = window.gdScene.add.graphics()
     .setScrollFactor(0)
     .setDepth(20);
 
   window.gdScene._player._showHitboxes = true;
-
-  // I honestly, need an onUpdate hook, as this lags 1 frame behind.
-  /* window.gdScene.events.on("update", () => {
-    window.gdScene._player.drawHitboxes(hitboxGraphics, window.gdScene._cameraX, window.gdScene._cameraY);
-  }); */
 });
 
-api.patchMethod("syncSprites", (code) => {
-  return code.slice(0, -1) +
-    ";this.drawHitboxes(window.hitboxGraphics, window.gdScene._cameraX, window.gdScene._cameraY)}";
+api.onUpdate(() => {
+  window.gdScene._player.drawHitboxes(
+    hitboxGraphics,
+    window.gdScene._cameraX,
+    window.gdScene._cameraY,
+  );
 });
 
 api.patchMethod("getNearbySectionObjects", (code) => {
